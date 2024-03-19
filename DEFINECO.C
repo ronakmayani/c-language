6c40209e7969b462c6d41e2e09c61df014546587
ronakmayani <163960958+ronakmayani@users.noreply.github.com>
2024-03-19 09:10:54 +0530

    Add files via upload

---
+++ DEFINECO.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+	int A=4,B=6,r=2,h=3,w=5,l=7;
+	const float pi=3.14;
+	clrscr();
+	printf("\n value of circle is %f",2*r*r);
+	printf( " \n value of rectangle is%d",w*l);
+	printf("\n value of triangle is %f2" ,0.5*h*w);
+	printf( " \n value of perimeter of the circle is %f",2*pi*r);
+	getch();
+
+}
---
+++ RONAK.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+	int a=12,b=6,c,d,e,f,g;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+
+
+
+	clrscr();
+	printf("\n value of A is %d",c);
+	printf( " \n value of b is %d" ,d);
+	printf( " \n value of c is %d" ,e);
+	printf( " \n value of d is %d" ,f);
+	printf( " \n value of e is %d" ,g);
+	getch();
+	}
